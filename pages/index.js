0ef18acac94331a66fd6b3893654aadb20c0e376
Zeljko Dakic <zeljko@dakic.com>
2021-04-19 01:26:43 -0500

    nice and centered

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,19 @@
 import Head from 'next/head'
+import styles from '../styles/home.module.css'
 
 export default function Home() {
   return (
-    <div>
+    <div className={styles.container}>
       <Head>
         <title>RPG::MMO::Game::In Space!</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main>
+      <main className={styles.main}>
         <h1>RPG::MMO::Game::In Space!</h1>
       </main>
 
-      <footer>
+      <footer className={styles.footer}>
       </footer>
     </div>
   )
