7d1ccae1cc1bdfa9a6b579b01e4f54cf6dc08033
Roger <roger.edlopez@gmail.com>
2021-04-28 23:21:54 -0600

    WIP: attributes UI

---
+++ components/common/ProgressBar.js
@@ -0,0 +1,11 @@
+const ProgressBar = () => {
+  return (
+    <div class="shadow w-full bg-gray-200">
+      <div className="bg-blue-500 text-xs py-1 text-center text-white w-3/4">
+        45%
+      </div>
+    </div>
+  );
+};
+
+export default ProgressBar;
--- pages/journey.js
+++ pages/journey.js
@@ -97,14 +97,159 @@ const JourneyPage = () => {
           <div className={styles.star} />
           <div className={styles.star2} />
           <div className={styles.star3} />
-          <div className="flex flex-col justify-between w-full">
-            <div className="flex">{renderSpaceBody()}</div>
+          <div className="flex flex-col w-full justify-between">
+            <div className="flex self-start h-32 w-32">{renderSpaceBody()}</div>
 
             <img
               alt="spaceship"
               src="./ship3.png"
-              className="h-32 w-32 transform -rotate-90 scale-150 mx-auto self-end mb-20"
+              className="h-32 w-32 transform -rotate-90 scale-150 mx-auto self-center"
             />
+
+            <div className="h-56 w-full bg-red-500 self-end flex flex-col">
+              <div className="border-b-2 border-gray-200 border-solid">
+                <ul className="flex cursor-pointer">
+                  <li className="py-2 px-6 bg-white rounded-t-md text-black mr-0.5 w-full">
+                    Attributes
+                  </li>
+                  <li className="py-2 px-6 bg-gray-400 rounded-t-md text-gray-500 mr-0.5 w-full">
+                    Ship
+                  </li>
+                  <li className="py-2 px-6 bg-gray-400 rounded-t-md text-gray-500 mr-0.5 w-full">
+                    Storage
+                  </li>
+                  <li className="py-2 px-6 bg-gray-400 rounded-t-md text-gray-500 mr-0.5 w-full">
+                    Craft
+                  </li>
+                </ul>
+              </div>
+
+              <div className="flex flex-1">
+                <div className="flex flex-col flex-1 bg-white text-black p-2">
+                  <p className="text-gray-900 mb-3">Ship attributes</p>
+
+                  <div className="flex">
+                    <div className="flex flex-col mr-4">
+                      <p className="text-black mb-1">Attack</p>
+                      <p className="text-black mb-1">Defense</p>
+                      <p className="text-black mb-1">Speed</p>
+                      <p className="text-black mb-1">A.I.</p>
+                    </div>
+                    <div className="flex flex-col flex-1">
+                      <div className="flex mb-1">
+                        <p>0</p>
+
+                        <button className="h-6 w-10 bg-blue-600 mx-2 rounded text-white">
+                          +
+                        </button>
+
+                        <button className="h-6 w-6 bg-yellow-500 rounded-full">
+                          i
+                        </button>
+                      </div>
+
+                      <div className="flex mb-1">
+                        <p>0</p>
+
+                        <button className="h-6 w-10 bg-blue-600 mx-2 rounded text-white">
+                          +
+                        </button>
+
+                        <button className="h-6 w-6 bg-yellow-500 rounded-full">
+                          i
+                        </button>
+                      </div>
+
+                      <div className="flex mb-1">
+                        <p>0</p>
+
+                        <button className="h-6 w-10 bg-blue-600 mx-2 rounded text-white">
+                          +
+                        </button>
+
+                        <button className="h-6 w-6 bg-yellow-500 rounded-full">
+                          i
+                        </button>
+                      </div>
+
+                      <div className="flex mb-1">
+                        <p>0</p>
+
+                        <button className="h-6 w-10 bg-blue-600 mx-2 rounded text-white">
+                          +
+                        </button>
+
+                        <button className="h-6 w-6 bg-yellow-500 rounded-full">
+                          i
+                        </button>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+
+                <div className="flex flex-col flex-1 bg-white text-black p-2">
+                  <p className="text-gray-900 mb-3">Your attributes</p>
+
+                  <div className="flex">
+                    <div className="flex flex-col mr-4">
+                      <p className="text-black mb-1">Intelligence</p>
+                      <p className="text-black mb-1">Social</p>
+                      <p className="text-black mb-1">Repair(?)</p>
+                      <p className="text-black mb-1">Fortune(luck)</p>
+                    </div>
+                    <div className="flex flex-col flex-1">
+                      <div className="flex mb-1">
+                        <p>-10</p>
+
+                        <button className="h-6 w-10 bg-blue-600 mx-2 rounded text-white">
+                          +
+                        </button>
+
+                        <button className="h-6 w-6 bg-yellow-500 rounded-full">
+                          i
+                        </button>
+                      </div>
+
+                      <div className="flex mb-1">
+                        <p>0</p>
+
+                        <button className="h-6 w-10 bg-blue-600 mx-2 rounded text-white">
+                          +
+                        </button>
+
+                        <button className="h-6 w-6 bg-yellow-500 rounded-full">
+                          i
+                        </button>
+                      </div>
+
+                      <div className="flex mb-1">
+                        <p>0</p>
+
+                        <button className="h-6 w-10 bg-blue-600 mx-2 rounded text-white">
+                          +
+                        </button>
+
+                        <button className="h-6 w-6 bg-yellow-500 rounded-full">
+                          i
+                        </button>
+                      </div>
+
+                      <div className="flex mb-1">
+                        <p>0</p>
+
+                        <button className="h-6 w-10 bg-blue-600 mx-2 rounded text-white">
+                          +
+                        </button>
+
+                        <button className="h-6 w-6 bg-yellow-500 rounded-full">
+                          i
+                        </button>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
 
